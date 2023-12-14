4ccdfad85519783e2a804bdb3540c537d0eaede3
Sergio Ramos <51498566+Sergioarg@users.noreply.github.com>
2023-12-13 22:39:48 -0500

    New folder

--- references_borrow/src/main.rs
+++ references_borrow/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let mut s = String::from("Hello");
 
-    let r1 = &s; // no problem // inmutable
-    let r2 = &s; // no problem // inmutable
+    let r1 = &s; // borrow // inmutable
+    let r2 = &s; // borrow // inmutable
     println!("{} and {}", r1, r2);
     // variables r1 and r2 will not be used after this point
 
-    let r3 = &mut s; // no problem // mutable
+    let r3 = &mut s; // borrow // mutable
     println!("{}", r3);
 }
