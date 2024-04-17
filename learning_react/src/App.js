b59c71f76de741f057eedce5236e2d83f0642a2e
sergioarg <sergio_ramos53@yahoo.com>
2024-04-17 16:29:46 -0500

    Practicing react

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Apples from "./components/Apples";
+import Bag from "./components/Bag";
+import Card from "./components/Card";
+import Heading from "./components/Heading";
+import Pears from "./components/Pears";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+    return (
+        <div className="App">
+            <h1>Task: Add three Card elements</h1>
+            <Card h2="First card's h2" h3="First card's h3" />
+            <Card h2="Second card's h2" h3="Second card's h3" />
+            <Card h2="Third card's h2" h3="Third card's h3" />
+
+            <Heading firstName="Bob"/>
+            <Bag children={<Apples color="yellow" number="5" />} />
+            <Bag children={<Pears friend="Peter" />} />
+        </div>
+    );
+};
 
 export default App;
