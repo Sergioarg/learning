b062a052e3833093b1af3f5a378b93351809cee4
sergioarg <sergio_ramos53@yahoo.com>
2024-05-04 14:39:06 -0500

    Calculator

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -1,37 +1,72 @@
-import Apples from "./components/Apples";
-import Bag from "./components/Bag";
-import Card from "./components/Card";
-import Heading from "./components/Heading";
-import Fruits from "./components/Fruits";
-import React from 'react';
-import FruitsCounter from './components/FruitsCounter';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-    const [fruits] = React.useState([
-        { fruitName: 'apple', id: 1 },
-        { fruitName: 'apple', id: 2 },
-        { fruitName: 'plum', id: 3 },
-    ]);
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
 
-    return (
-        <div className="App">
-            <nav>
-                <Link to="/" className='nav-item'>Home Page</Link>
-                <Link to="/bag" className='nav-item'>Bag </Link>
-            </nav>
-            <Routes>
-                <Route path='/' element={<Heading firstName='Home Page' />}></Route>
-                <Route path='/bag' element={<Bag children={<Apples color="yellow" number="5" />} />}></Route>
-            </Routes>
+  function plus(e) {
+    e.preventDefault();
+    // const inputVal = inputRef.current.value;
+    // const newResult = result + Number(inputVal);
+    // setResult(newResult);
+    setResult((result) => result + Number(inputRef.current.value));
+  }
 
-            <h1>Where should the state go?</h1>
-            <Fruits fruits={fruits} />
-            <FruitsCounter fruits={fruits} />
+  function minus(e) {
+    e.preventDefault();
+    const inputVal = inputRef.current.value;
+    const newResult = result - Number(inputVal);
+    setResult(newResult);
+  }
 
-            <Card h2="First card's h2" h3="First card's h3" />
-        </div>
-    );
-};
+  function times(e) {
+    e.preventDefault();
+    const inputVal = inputRef.current.value;
+    const newResult = result * Number(inputVal);
+    setResult(newResult);
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    const inputVal = inputRef.current.value;
+    const newResult = result / Number(inputVal);
+    setResult(newResult);
+  }
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  }
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button>
+      </form>
+    </div>
+  );
+}
 
 export default App;
